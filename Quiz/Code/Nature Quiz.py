cd672544a5992a3affc1d2b5fb84515a0eae8626
Odoxical <owewou02@kingshigh.school.nz>
2026-04-02 20:24:22 +1300

    score saving done

--- Quiz/Code/Nature Quiz.py
+++ Quiz/Code/Nature Quiz.py
@@ -1,11 +1,36 @@
 import FreeSimpleGUI as sg
+import json
 ########################################################################################################################################################################################################
-def save():
-    pass
+def save(correct_answers,historical_correct_answers):
+    try:
+        if correct_answers>historical_correct_answers:
+            historical_correct_answers=correct_answers
+            with open("highscore.txt","w") as file:
+                file.write()
+            return historical_correct_answers
+        else:
+            pass
+    except TypeError:
+        historical_correct_answers=correct_answers
+        with open("highscore.txt","w") as file:
+            file.write(str(historical_correct_answers))
+        return historical_correct_answers
+    
 def load_score():
-    pass
+    try:
+        with open("highscore.txt","r") as file:
+            return int(file.read())
+    except FileNotFoundError:
+            return 0
+            print("But nothing came")
 ########################################################################################################################################################################################################
-
+try:
+    with open("questions.json","r") as file:
+        questions = json.load(file)
+except json.JSONDecodeError:
+    sg.popup(f"I am sorry, but the list of questions has been corrupted", title="Final Score", background_color='#0e3947')
+except FileNotFoundError:
+    sg.popup(f"I am sorry but something has gone very wrong. the list of questions cannot be found", title="Final Score", background_color='#0e3947')
 questions = {1:{"question_text":"Which of these animals has the most lethal venom",
                 "type":"four",
                 "answers":["Snake","Spider","JellyFish","Snail"],
@@ -88,40 +113,41 @@ This tree is called the Dynamite Tree."""
 
 
 ########################################################################################################################################################################################################
-
 will_iterate = 1
 answer_chosen=False
 current_question = questions[will_iterate]
-answers = current_question["answers"] #I had completely forgot about this bit of code. Thank you, me. or is it just thank me? an intresting conundrum.
-historical_correct_answers=0
+answers = current_question["answers"]
+historical_correct_answers=load_score()
 correct_answers=0
+load_score()
 
 menu = [
-[sg.pin(sg.Button("Play Quiz", font=("calibri",25, "bold")))], 
-[sg.Button("Score", font=("calibri",20, "bold"))],
-[sg.pin(sg.Button("leave", font=("calibri",2, "bold")))],
+[sg.Button("Play Quiz", font=("calibri",25, "bold") ), 
+sg.Button("See Highscore", font=("calibri",20, "bold"))],
+[sg.Button("leave", font=("calibri",20, "bold")),
+sg.Button("Clear Highscore", font=("calibri",20, "bold"))],
 ]
 
 four_buttons = [ 
-[sg.Text(current_question["question_text"], font=("calibri",20,  "bold"), key = "-FOUR_QUESTION_TEXT-")],
-[sg.Button("If you're seeing this, the question iteration code broke", key="-BUTTON1-", font=("calibri", 25), size=(40,5), button_color=('#000000','#B5FA42')),
-sg.Button("If you're seeing this, the question iteration code broke", font=("calibri", 25), key="-BUTTON2-", button_color=('#000000','#42FAE3') , size=(40,5))],
-[sg.Button("If you're seeing this, the question iteration code broke", key="-BUTTON3-", font=("calibri", 25),button_color=('#000000','#8742FA') ,size=(40,5)),
-sg.Button("If you're seeing this, the question iteration code broke", font=("calibri", 25), key="-BUTTON4-", size=(40,5), button_color=('#000000','#FA4259'))],
-[sg.Text("", key="-FOUR_QUESTION_TRIVIA-",font=("calibri"),size=(80,12))],
+[sg.Text(current_question["question_text"], font=("calibri",20,  "bold"), key = "-FOUR_QUESTION_TEXT-", background_color='#0e3947')],
+[sg.Button("If you're seeing this, the question iteration code broke", key="-BUTTON1-", font=("calibri", 25), size=(40,5), button_color=('#000000','#B5FA42'), border_width = (0)),
+sg.Button("If you're seeing this, the question iteration code broke", font=("calibri", 25), key="-BUTTON2-", button_color=('#000000','#42FAE3') , size=(40,5), border_width = (0))],
+[sg.Button("If you're seeing this, the question iteration code broke", key="-BUTTON3-", font=("calibri", 25),button_color=('#000000','#8742FA') ,size=(40,5), border_width = (0)),
+sg.Button("If you're seeing this, the question iteration code broke", font=("calibri", 25), key="-BUTTON4-", size=(40,5), button_color=('#000000','#FA4259'), border_width = (0))],
+[sg.Text("", key="-FOUR_QUESTION_TRIVIA-",font=("calibri"),size=(80,12), background_color='#0e3947')],
 
 ]
 true_or_false = [
-    [sg.Text(current_question["question_text"], font=("calibri",20,  "bold"), key = "-TRUE_FALSE_TEXT-")],
-    [sg.Button("", key="-TRUE_OR_FALSE_BUTTON1-", font=("calibri", 25), size=(40,15), button_color=('#000000','#B5FA42')),
-    sg.Button("", key="-TRUE_OR_FALSE_BUTTON2-", font=("calibri", 25), size=(40,15), button_color=('#000000','#8742FA'))],
-    [sg.Text("", key="-TRUE_FALSE_TRIVIA-")]
+    [sg.Text(current_question["question_text"], font=("calibri",20,  "bold"), key = "-TRUE_FALSE_TEXT-", background_color='#0e3947')],
+    [sg.Button("", key="-TRUE_OR_FALSE_BUTTON1-", font=("calibri", 25), size=(40,15), button_color=('#000000','#B5FA42'), border_width = (0)),
+    sg.Button("", key="-TRUE_OR_FALSE_BUTTON2-", font=("calibri", 25), size=(40,15), button_color=('#000000','#8742FA'), border_width = (0))],
+    [sg.Text("", key="-TRUE_FALSE_TRIVIA-", background_color='#0e3947')]
     ]
 
 master_layout = [
-    [sg.Column(four_buttons,key = "-FOUR_QUESTION-", visible = False),
-    sg.Column(true_or_false,key = "-TRUE_FALSE-", visible = False), #####
-    sg.Column(menu, key= "-MENU-", visible = True)]
+    [sg.Column(four_buttons,key = "-FOUR_QUESTION-", visible = False, background_color='#0e3947'),
+    sg.Column(true_or_false,key = "-TRUE_FALSE-", visible = False, background_color='#0e3947'), #####
+    sg.Column(menu, key= "-MENU-", visible = True, background_color='#0e3947')]
     ]
 
 
@@ -148,6 +174,8 @@ def load():
         window["-FOUR_QUESTION-"].update(visible=True)
     else:
         window["-TRUE_FALSE_TEXT-"].update(current_question["question_text"])
+        window["-TRUE_OR_FALSE_BUTTON1-"].update(answers[0])
+        window["-TRUE_OR_FALSE_BUTTON2-"].update(answers[1])
         window["-MENU-"].update(visible=False)
         window["-TRUE_FALSE-"].update(visible=True)
         window["-FOUR_QUESTION-"].update(visible=False)
@@ -176,7 +204,7 @@ waiting = False
 while running == True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == "leave":
-        save()
+        save(correct_answers,historical_correct_answers)
         exit()
     elif event == "Play Quiz": #####
         will_iterate=1
@@ -184,7 +212,11 @@ while running == True:
         waiting = False
         load()
         print(will_iterate)
-    elif event == "Score":
+    elif event == "See Highscore":
+        historical_correct_answers = load_score()
+        sg.popup(f"The high score is:{historical_correct_answers}", title="High Score", background_color='#0e3947')
+    elif event == "Clear Highscore":
+        save(0,-1)
         load_score()
     elif event in ["-BUTTON1-", "-BUTTON2-", "-BUTTON3-", "-BUTTON4-"] and not waiting:
         index = int(event[-2]) -1
@@ -209,7 +241,8 @@ while running == True:
             window["-FOUR_QUESTION-"].update(visible=False)
             window["-TRUE_FALSE-"].update(visible=False)
             window["-MENU-"].update(visible=True)
-            sg.popup(f"Quiz complete! You got {correct_answers} out of {len(questions)} correct!", title="Final Score")
+            sg.popup(f"Quiz complete! You got {correct_answers} out of {len(questions)} correct!", title="Final Score", background_color='#0e3947')
+            save(correct_answers,historical_correct_answers)
                     
 
 
